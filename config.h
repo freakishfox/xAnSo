84ab2df984ad54e2722bdd0e5f7a22e4ae69f83f
nansong <nansong@mogujie.com>
2017-05-22 23:24:37 +0800

    df -> 增加编译配置文件

---
+++ config.h
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ *  @file      config.h 2017\5\22 21:30:13 $
+ *  @author    df
+ *  @brief     
+ ******************************************************************************/
+
+#ifndef CONFIG_8179D109_BF00_49AA_9FEC_F6B7213499A7_H__
+#define CONFIG_8179D109_BF00_49AA_9FEC_F6B7213499A7_H__
+
+#ifdef _WIN32
+#define WINDOWS_BUILD
+
+#endif
+
+#define delay_print_interval 20
+
+#endif// CONFIG_8179D109_BF00_49AA_9FEC_F6B7213499A7_H__
